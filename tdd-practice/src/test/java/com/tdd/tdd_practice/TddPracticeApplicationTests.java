bde01214c0e1907b744707ee4a478d5552930cac
Hoo-sung <gntjd135@gmail.com>
2024-08-13 15:31:53 +0900

    init: first commit

---
+++ tdd-practice/src/test/java/com/tdd/tdd_practice/TddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.tdd_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
